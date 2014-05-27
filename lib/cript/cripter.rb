be82d85165df63d41393268a771ce2f0adc1414c
Andrew Tongen <atongen@gmail.com>
2014-05-26 20:11:42 -0500

    Pull pub key from priv key if available

--- lib/cript/cripter.rb
+++ lib/cript/cripter.rb
@@ -19,22 +19,32 @@ module Cript
     # private_key_path
     # passphrase
     def initialize(options = {})
-      @opt = options
+      @options = options
 
-      unless [:public_key_content, :private_key_content, :public_key_path, :private_key_path].any? { |o| @opt[o] }
+      # Attempt to use the private key at the default location
+      # if exists and not otherwise specified
+      unless [:private_key_content, :private_key_path].any? { |o| @options[o] }
         if File.file?("#{ENV['HOME']}/.ssh/id_rsa")
-          @opt[:private_key_path] = "#{ENV['HOME']}/.ssh/id_rsa"
-        end
-        if File.file?("#{ENV['HOME']}/.ssh/id_rsa.pub")
-          @opt[:public_key_path] = "#{ENV['HOME']}/.ssh/id_rsa.pub"
+          @options[:private_key_path] = "#{ENV['HOME']}/.ssh/id_rsa"
         end
       end
 
-      if [:private_key_content, :private_key_path].any? { |o| @opt[o] }
-        @private_key = OpenSSL::PKey::RSA.new(*[key_content(:private), @opt.delete(:passphrase)])
+      if @options[:private_key_content]
+        @private_key = OpenSSL::PKey::RSA.new(*[@options[:private_key_content], @options.delete(:passphrase)])
+      elsif @options[:private_key_path] && File.file?(@options[:private_key_path])
+        @private_key = OpenSSL::PKey::RSA.new(*[File.read(@options[:private_key_path]), @options.delete(:passphrase)])
       end
-      if [:public_key_content, :public_key_path].any? { |o| @opt[o] }
-        @public_key = OpenSSL::PKey::RSA.new(key_content)
+
+      if @private_key
+        @public_key = private_key.public_key
+      else
+        if @options[:public_key_content]
+          @public_key = OpenSSL::PKey::RSA.new(@options[:public_key_content])
+        elsif @options[:public_key_path] && File.file?(@options[:public_key_path])
+          @public_key = OpenSSL::PKey::RSA.new(File.read(@options[:public_key_path]))
+        elsif File.file?("#{ENV['HOME']}/.ssh/id_rsa.pub")
+          @public_key = OpenSSL::PKey::RSA.new(File.read("#{ENV['HOME']}/.ssh/id_rsa.pub"))
+        end
       end
     end
 
@@ -52,37 +62,6 @@ module Cript
 
     def echo(message)
       decrypt(encrypt(message))
-    end
-
-    private
-
-    def key_content(type = :public)
-      type = :private unless type == :public
-      if @opt[:"#{type}_key_content"]
-        @opt[:"#{type}_key_content"]
-      elsif @opt[:"#{type}_key_path"]
-        content = File.read(@opt[:"#{type}_key_path"])
-        if content.include?("#{type.to_s.upcase} KEY")
-          content
-        else
-          ssh_key_to_pem(@opt[:"#{type}_key_path"])
-        end
-      else
-        raise Cript::Cripter::Error, "No #{type} key content"
-      end
-    end
-
-    def ssh_key_to_pem(path)
-      ssh_keygen("-f \"#{path}\" -e -m pem")
-    end
-
-    def ssh_keygen(cmd)
-      ssh_keygen = %x{ which ssh-keygen }.to_s.strip
-      if ssh_keygen != ""
-        %x{ #{ssh_keygen} #{cmd} }.to_s.strip
-      else
-        raise "ssh-keygen not available"
-      end
     end
 
   end
